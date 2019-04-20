159a3f871b9874dd0c4985ee8d5be5f413ed0b36
RockNRoll <himera80@mail.ru>
2019-04-20 14:19:02 +0300

    test-2

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+var i=0;
+while (i<20) {
+	for(var j=1; j<i;j++)
+		document.write("*");
+		document.write("<br>");
+	i++;
+}
+*/
+/*
+var m = [11, 25, 1, 88, 64, 3, 84, 7];
+ var min = m[0];
+var min_ind = 0;
+ for(var i = 1; i<m.length; i++) {
+	 if(m[i] < min) {
+	    min = m[i];
+		 min_ind = i;
+
+	    } document.write(m[i] + " ");
+ } 	document.write( "<br>m_id " + min);
+	document.write( "<br>adad " + min_ind);*/
